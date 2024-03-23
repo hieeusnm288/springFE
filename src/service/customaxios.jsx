d64443d30014939de3bbaa400de1782c93ec2a4e
Nguyen Hieu <nguuyenminhhieu28801@gmail.com>
2024-03-23 12:16:31 +0700

    Service

--- src/service/customaxios.jsx
+++ src/service/customaxios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://springbe-production.up.railway.app/api/v1",
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://springbe-production.up.railway.app/api/v1",
+  // baseURL: "http://localhost:8080/api/v1",
   timeout: 5000,
 });
 
